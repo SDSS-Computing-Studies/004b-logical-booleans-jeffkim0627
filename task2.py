041e699643ffe6102f3c161079dee0b675a7317c
jeffkim0627 <55156837+jeffkim0627@users.noreply.github.com>
2020-10-13 12:33:05 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -19,16 +19,16 @@ Enter a number: 8
 8 is only a perfect cube.
 """
 import math
-number = float(input("Enter a number"));                                                                                         numbers = 720
+number = float(input("Enter a number"))
 
-if (number**(1/2) == math.ceil(number**(1/2))) and (number**(1/3) != math.ceil(number**(1/3))):
+if round(number**(1/2), 13) == math.ceil(number**(1/2)) and round(number**(1/3),13) != math.ceil(number**(1/3)):
     print(str(number) + " is only a perfect square")
 
 elif (number**(1/3) == math.ceil(number**(1/3))) and (number**(1/2) != math.ceil(number**(1/2))):
     print(str(number) + " is only a perfect cube")
 
-elif (number**(1/2) == math.ceil(number**(1/2))) and (number**(1/3) == math.ceil(number**(1/3))):
-    print(str(numbers) + " is both a perfect square and a perfect cube")
+elif round(number**(1/2),13) == math.ceil(number**(1/2)) and round(number**(1/3),13) == math.ceil(number**(1/3)):
+    print(str(number) + " is both a perfect square and a perfect cube")
 
 elif (number**(1/2) != math.ceil(number**(1/2))) and (number**(1/3) != math.ceil(number**(1/3))) :
     print(str(number) + " is not a perfect square and a perfect cube")
